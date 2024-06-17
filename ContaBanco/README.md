03b924146a7aacfc36864005262c76473fd8f5fa
Luzapedreira <luza.freitas@gmail.com>
2024-06-17 14:21:34 -0300

    sistemabancário funcionando

--- ContaBanco/README.md
+++ ContaBanco/README.md
@@ -1,18 +1,8 @@
-## Getting Started
+## Desafio de projeto
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Criação de um sistema bancário básico em Java.
 
-## Folder Structure
+## Objetivos:
 
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Receberá dados via terminal contendo as características de conta em banco;
+O usuário receberá a mensagem de qual informação será solicitada;
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+
+        String nomeCliente;
+        int contaCliente;
+        String agencia;
+        double saldoConta;
+
+        System.out.println("Para abertura de conta, por gentileza preencha os campos conforme solicitado.");
+
+        System.out.println("Digite seu nome completo: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o número da sua conta: ");
+        contaCliente = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o número da agência correspondente: ");
+        agencia = scanner.nextLine();
+             
+        System.out.println("Digite o valor do valor a ser depositado: ");
+        saldoConta = scanner.nextDouble();
+
+        String conclusao = "Olá " + nomeCliente + ",\n" +
+        "Obrigado por criar uma conta em nosso banco! " +
+        "Sua agência é " + agencia + ", conta " + contaCliente + "\n" +
+        "Seu saldo inicial de R$" + saldoConta + " já está disponível para saque.";
+
+// Exibição da mensagem final
+System.out.println("\n" + conclusao); 
     }
     
+
 }
